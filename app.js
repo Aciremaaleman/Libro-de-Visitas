a720ec4c7d2a6fab374289fd84dfc52cfff856ce
aciremaaleman <aciremaaleman@outlook.es>
2018-02-14 23:44:15 -0600

    Agregando funcionalidad js v1

--- app.js
+++ app.js
@@ -1,29 +1,73 @@
-var $textOne = $("#textarea_one");
-var $textTwo = $("#text_two");
-var $submitText = $("#submit");
-
+//Se mandan a llamar los elementos por medio del id para el textarea
+var $comment = $("#comment");
+// Se mandan a llamar el elemento por medio del id para el lugar donde se mostrara el texto que viene del textarea
+var $commentShow = $("#print");
+//Se mandan a llamar los elementos por medio del id para los botones que daran estilo
+var $lg = $("#large");
+var $md = $("#medium");
+var $sm = $("#small");
+var $cTxt = $("#colorText");
+var $cF = $("#colorFondo");
+var $lft = $("#izquierda");
+var $ctr = $("#centrar");
+var $rgt = $("#derecha");
+// Se mandan a llamar el elemento por medio del id para subir el texto
+var $btn = $("#submit");
+// Se agrega el comentario al lugar indicado
+// var $postComment = $("#post");
+// Se inicializa jquery
 $(document).ready(function() {
-  $textOne.keyup(enter);
-  $textTwo.keyup(paint);
-  $submitText.click(paint);
+  // se agrega el evento al textarea para comenzar la funcion de obtener el valor.
+  $comment.keyup(getText);
+  // Se agrega el evento click que dara el estilo en especifico.
+  $lg.click(sizeLarge);
+  $md.click(sizeMd);
+  $sm.click(sizeSm);
+  $cTxt.click(colorTx);
+  $cF.click(colorF);
+  $lft.click(textLeft);
+  $ctr.click(textCenter);
+  $rgt.click(textRight);
+  // Boton para postear comentario final
+  $btn.click(postComment);
+
 
 });
 
-function enter(){
-  var $text = $textOne.val().trim();
-  console.log($text);
-  paint($text);
+function getText(e){
+  e.preventDefault();
+  var $text = $comment.val().trim();
+  $commentShow.text($text);
 };
 
-function paint($text){
-  var $container = $('<div class="row"> </div>');
-  var $div = $('<div></div>');
-  var $p = $("<p 'id=text_two'></p>");
+function sizeLarge(){
+  $("#print").attr('class','large');
 
-  $p.text($text);
-  $container.append($div);
-  $container.append($p);
-  $("#text_two").prepend($container);
+};
+function sizeMd(){
+  $("#print").attr('class','medium');
+};
+function sizeSm(){
+  $("#print").attr('class','small');
+};
+function colorTx(){
+  $("#print").attr('class','colorText');
+};
+function colorF(){
+  $("#print").attr('class','colorBack');
+};
+function textLeft() {
+  $("#print").attr('class','left');
+};
+function textCenter() {
+  $("#print").attr('class','center');
+};
+function textRight() {
+  $("#print").attr('class','right');
+};
 
-
+function postComment(e){
+  e.preventDefault();
+  var $textComment = $commentShow.val().trim();
+  $("#post").text($textComment);
 };
